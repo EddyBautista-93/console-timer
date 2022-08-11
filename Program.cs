a623174f06e2fb618a53c00ba811ea1cb7ca5959
EddyBautista <bautista.j.eddy@gmail.com>
2022-08-11 17:53:13 -0500

    add gitignore

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace console_timer
+{
+    class Program
+    {
+        // TODO: Create a MVP Timer in the terminal and add features from there
+        // Task:
+
+        // Create a timer and have it work in the terminal when ran
+
+        public static void Timer()
+        {
+            int seconds = 60;
+            int minutes = 25;
+            for(seconds = 60; seconds >=0; seconds--)
+            {
+                Console.SetCursorPosition(0,2);
+                Console.Write("Generating Preview in {0}:{1}",minutes, seconds);
+                System.Threading.Thread.Sleep(1000);
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Timer();
+        }
+    }
+}
